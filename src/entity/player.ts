9cc4b08c65fb16674c33c159d6841b5d9ab63430
Aotokitsuruya <contact@aotoki.me>
2023-11-14 23:32:53 +0800

    Implement Position class

--- src/entity/player.ts
+++ src/entity/player.ts
@@ -1,12 +1,4 @@
-export class Position {
-  public readonly x: number;
-  public readonly y: number;
-
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
-}
+import { Position } from "./position";
 
 export class Player {
   public readonly id: string;
---
+++ src/entity/position.ts
@@ -0,0 +1,13 @@
+export class Position {
+  public readonly x: number;
+  public readonly y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(position: Position): Position {
+    return new Position(this.x + position.x, this.y + position.y);
+  }
+}
