b89ace32064fdcf105406a7bf0d4654406403d2a
Aotokitsuruya <contact@aotoki.me>
2023-11-19 15:19:50 +0800

    Attach physic body to player

--- src/repository/players.ts
+++ src/repository/players.ts
@@ -1,16 +1,37 @@
 import { injectable, inject } from "inversify";
-import { Player } from "../entity";
 import { Subject } from "rxjs";
-import { Event, EventBusSymbol, PlayerUpdated } from "../event";
+import { Engine, Bodies, Body, Composite } from "matter-js";
+import { Player } from "@/entity";
+import { Event, EventBusSymbol, PlayerUpdated } from "@/event";
 
 @injectable()
 export class Players {
-  private events: Subject<Event>;
+  private readonly events: Subject<Event>;
+  private readonly physEngine: Engine;
   private player: Player;
+  private playerBody;
 
-  constructor(@inject(EventBusSymbol) events: Subject<Event>) {
+  constructor(
+    @inject(EventBusSymbol) events: Subject<Event>,
+    @inject(Engine) physEngine: Engine,
+  ) {
     this.events = events;
+    this.physEngine = physEngine;
+    /**
+     * Static Player
+     */
     this.player = new Player("1");
+    this.playerBody = Bodies.rectangle(
+      this.player.position.x,
+      this.player.position.y,
+      16,
+      16,
+      {
+        isStatic: true,
+      },
+    );
+
+    Composite.add(this.physEngine.world, this.playerBody);
   }
 
   find() {
@@ -19,6 +40,7 @@ export class Players {
 
   save(player: Player) {
     this.player = player;
+    Body.setPosition(this.playerBody, player.position);
     this.events.next(new PlayerUpdated(player.id));
   }
 }
