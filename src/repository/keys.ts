6960cd855e20d1321a5ab9bb8125fa697561e447
Aotokitsuruya <contact@aotoki.me>
2023-11-19 16:30:37 +0800

    Bound physic body to Key

--- src/repository/keys.ts
+++ src/repository/keys.ts
@@ -3,8 +3,10 @@ import { Subject } from "rxjs";
 import { Key } from "@/entity";
 import { Event, EventBusSymbol, KeyUpdated } from "@/event";
 import { v4 as uuidv4 } from "uuid";
+import { Engine, Body, Bodies, Composite } from "matter-js";
 
 const keys: Key[] = [];
+const bodies: Body[] = [];
 
 @injectable()
 export class ListKeys {
@@ -16,9 +18,14 @@ export class ListKeys {
 @injectable()
 export class Keys {
   private readonly events: Subject<Event>;
+  private readonly physEngine: Engine;
 
-  constructor(@inject(EventBusSymbol) events: Subject<Event>) {
+  constructor(
+    @inject(EventBusSymbol) events: Subject<Event>,
+    @inject(Engine) physEngine: Engine,
+  ) {
     this.events = events;
+    this.physEngine = physEngine;
   }
 
   find(id: string) {
@@ -27,6 +34,10 @@ export class Keys {
 
   save(key: Key) {
     keys.push(key);
+    const body = Bodies.rectangle(key.position.x, key.position.y, 16, 16);
+    bodies.push(body);
+    Composite.add(this.physEngine.world, body);
+
     this.events.next(new KeyUpdated(uuidv4()));
   }
 }
